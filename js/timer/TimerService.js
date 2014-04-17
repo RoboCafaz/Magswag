fcf83e0ba7c2cd37b372cccbce559e491efe1ccf
robocafaz <robocafaz@gmail.com>
2014-04-16 22:16:23 -0400

    Adding clock directive and timer addon.

--- js/timer/TimerService.js
+++ js/timer/TimerService.js
@@ -8,7 +8,19 @@ angular.module('MagTimer', [])
 		templateUrl : 'views/directives/timer/eventList.html',
 		replace : true,
 		link : function ($scope, $element, $attrs) {
-			
+			console.debug("Sweg");
+		}
+	}
+})
+.directive("clock", function () {
+	return {
+		restrict : 'E',
+		template : "{{now | date: 'medium'}}",
+		link : function ($scope, $element, $attrs) {
+			setInterval(function () {
+				$scope.now = new Date().getTime();
+				$scope.$apply();
+			}, 500);
 		}
 	}
 });
